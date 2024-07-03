aac15e4a965a991d77e8a7cc2664bcf798410bf7
ape-with-helmet <ganeshshatrugna@gmail.com>
2024-07-03 15:30:43 +0530

    HMM

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 import requests
 from io import BytesIO
+import validators
 
 app = Flask(__name__)
 model = load_model('models/my_model.keras')
@@ -13,7 +14,10 @@ class_labels = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 
 
 def load_and_preprocess_image_from_url(img_url):
     try:
+        if not validators.url(img_url):
+            raise ValueError("Invalid URL")
         response = requests.get(img_url)
+        response.raise_for_status()  # Raise HTTPError for bad responses
         img = Image.open(BytesIO(response.content))
         img = img.resize((32, 32))
         img_array = image.img_to_array(img)
@@ -31,15 +35,21 @@ def index():
 
 @app.route('/classify', methods=['POST'])
 def classify_image():
-    img_url = request.json.get('imgUrl', '')
-    img_array = load_and_preprocess_image_from_url(img_url)
-    if img_array is not None:
-        prediction = model.predict(img_array)
-        predicted_class = np.argmax(prediction)
-        result = class_labels[predicted_class]
-        return jsonify({'result': result})
-    else:
-        return jsonify({'result': 'Error: Failed to process the image from URL.'})
+    try:
+        img_url = request.json.get('imgUrl', '')
+        if not img_url:
+            return jsonify({'result': 'Error: No image URL provided.'}), 400
+        img_array = load_and_preprocess_image_from_url(img_url)
+        if img_array is not None:
+            prediction = model.predict(img_array)
+            predicted_class = np.argmax(prediction)
+            result = class_labels[predicted_class]
+            return jsonify({'result': result})
+        else:
+            return jsonify({'result': 'Error: Failed to process the image from URL.'}), 400
+    except Exception as e:
+        print(f"Error during classification: {e}")
+        return jsonify({'result': 'Error: Something went wrong during classification.'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
